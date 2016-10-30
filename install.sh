175982c5edc7a533afd4784438ba5e5054c1ec1b
oorococo <oorococo@gmail.com>
2016-10-30 15:14:30 +0800

    update

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
+# 安装 vim 插件管理
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# 建立 vim 配置符号链接
 ln -sf ~/home_dots/vim/.vimrc ~/.vimrc
+# 建立 tmux 配置符号链接
 ln -sf ~/home_dots/tmux/.tmux.conf ~/.tmux.conf
